a73c016a2ea09afd2993063b8577541ffc4cbfca
Chahd Letaief <chahd.ltf21@gmail.com>
2025-12-14 14:42:33 +0100

    fixing springboot

--- src/test/java/com/quickstartdev/librarymanagementsystem/ApplicationTests.java
+++ src/test/java/com/quickstartdev/librarymanagementsystem/ApplicationTests.java
@@ -3,11 +3,12 @@ package com.quickstartdev.librarymanagementsystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.knf.dev.librarymanagementsystem.Application;
+
+@SpringBootTest(classes = Application.class)
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
